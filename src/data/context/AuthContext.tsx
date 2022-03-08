c7e4bdd816ad06885570196f0cabe03736deb3c0
Edmilson Torres <edmilson.torres@gmail.com>
2022-03-08 12:51:56 -0300

    feat: auth persist with cookie session

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,5 +1,6 @@
+import { createContext, useEffect, useState } from "react";
 import firebase from "../../firebase/config";
-import { createContext, useState } from "react";
+import Cookies from "js-cookie";
 import User from "../../model/User";
 
 import route from "next/router";
@@ -18,25 +19,51 @@ async function userDefault(userFirebase: firebase.User): Promise<User> {
     name: userFirebase.displayName,
     email: userFirebase.email,
     token,
-    provider: userFirebase.providerData[0]?.providerId,
+    provider: userFirebase.providerData[0].providerId,
     imageUrl: userFirebase.photoURL,
   };
 }
 
+function cookieManager(logged: boolean) {
+  if (logged) {
+    Cookies.set("auth-logged", logged, { expires: 7 });
+  } else {
+    Cookies.remove("auth-logged");
+  }
+}
+
 export function AuthProvider(props) {
+  const [loading, setLoading] = useState(true);
   const [user, setUser] = useState<User>(null);
+
+  async function configSession(userFirebase) {
+    if (userFirebase?.email) {
+      const user = await userDefault(userFirebase);
+      setUser(user);
+      cookieManager(true);
+      setLoading(false);
+      return user.email;
+    } else {
+      setUser(null);
+      cookieManager(false);
+      setLoading(false);
+      return false;
+    }
+  }
 
   async function loginGoogle() {
     const resp = await firebase
       .auth()
       .signInWithPopup(new firebase.auth.GoogleAuthProvider());
 
-    if (resp.user?.email) {
-      const user = await userDefault(resp.user);
-      setUser(user);
-      route.push("/");
-    }
+    await configSession(resp.user);
+    route.push("/");
   }
+
+  useEffect(() => {
+    const cancel = firebase.auth().onIdTokenChanged(configSession);
+    return () => cancel();
+  }, []);
 
   return (
     <AuthContext.Provider
