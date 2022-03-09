be4f36aed9872eea3fd42504f44843f52434bb0d
Edmilson Torres <edmilson.torres@gmail.com>
2022-03-08 21:45:20 -0300

    feat: protected routes with HEAD script

--- src/components/auth/AuthForce.tsx
+++ src/components/auth/AuthForce.tsx
@@ -2,12 +2,28 @@ import Image from "next/image";
 import Router from "next/router";
 import loadingGif from "../../../public/assets/images/loading.gif";
 import useAuth from "../../data/hook/useAuth";
+import Head from "next/head";
 
 export default function AuthForce(props) {
   const { user, loading } = useAuth();
 
   function renderContent() {
-    return <>{props.children}</>;
+    return (
+      <>
+        <Head>
+          <script>
+            dangerouslySetInnerHTML=
+            {{
+              __html: `
+                if(!document.cookie?inclues("auth-logged")){
+                window.location.href = "/auth"}
+              `,
+            }}
+          </script>
+        </Head>
+        {props.children}
+      </>
+    );
   }
   function renderLoading() {
     return (
